f9bd549277f3437c61695267b0c7c49caae42aeb
Regina Giniyatova <giniyatovar@yandex.ru>
2024-07-23 17:12:59 +0300

    third game

--- Makefile
+++ Makefile
@@ -10,10 +10,18 @@ publish:
 lint:
 	npx eslint .
 
+lint-fix:
+	npx eslint . --fix
+
 brain-even:
 	node bin/brain-even.js
 
 brain-calc:
 	node bin/brain-calc.js
+
+brain-gcd:
+	node bin/brain-gcd.js
+
+
 
 
---
+++ src/games/gcd.js
@@ -0,0 +1,31 @@
+import randomNumber from '../randomNumber.js';
+import runManagment from '../index.js';
+
+const gcd = (a, b) => {
+  let firstNumber = a;
+  let secondNumber = b;
+
+  while (firstNumber !== 0 && secondNumber !== 0) {
+    if (firstNumber > secondNumber) {
+      firstNumber %= secondNumber;
+    } else {
+      secondNumber %= firstNumber;
+    }
+  }
+
+  return firstNumber + secondNumber;
+};
+
+const isGcdGame = () => {
+  const a = randomNumber() + 1;
+  const b = randomNumber() + 1;
+
+  const question = `${a} ${b}`;
+  const answer = gcd(a, b);
+
+  return [question, `${answer}`];
+};
+
+export default function runGcdGame() {
+  runManagment('Find the greatest common divisor of given numbers.', isGcdGame);
+}
